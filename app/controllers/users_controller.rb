d0c704586e62b5906daa35fea41ee9d3dc38f324
vagrant <stan.solomean@gmail.com>
2019-02-22 19:28:08 +0000

    updates to user model and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.user_id
+      redirect_to '/'
+    else
+      redirect_to '/sign_up'
+    end
   end
-  
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirm)
+    end
+
 end
