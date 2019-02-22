143e7fc02506ddb6af5c8f492c4c4080e3923567
vagrant <stan.solomean@gmail.com>
2019-02-22 23:12:25 +0000

    added mailer functionality and email text

--- app/mailers/receipt_mailer.rb
+++ app/mailers/receipt_mailer.rb
@@ -1,2 +1,7 @@
 class ReceiptMailer < ApplicationMailer
+
+  def receipt_email
+    @order = order
+    mail(to: @order.email, subject: Thank you for shopping with us! Order # #{@order.id})    
+  end
 end
