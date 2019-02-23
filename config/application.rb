71a2a793b494966aa0b418ec1493beb66c24231b
vagrant <stan.solomean@gmail.com>
2019-02-23 21:00:33 +0000

    added admin auth. couldnt figure out no implicit conversion of nil into String bug, so went ahead with non-modular authentication, should come back to fix later

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -28,6 +29,7 @@ module Jungle
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.read_encrypted_secrets = true
 
     config.generators do |g|
       g.skip_routes  true
